639ddb9783c9972a597da67edb8bff1f1e852c91
thallesaaguiar <thallesa97@gmail.com>
2021-12-12 18:22:55 -0300

    commit on master

--- Aula04/s3.tf
+++ Aula04/s3.tf
@@ -4,11 +4,22 @@ resource "aws_s3_bucket" "this" {
   tags = local.common_tags
 }
 
+resource "aws_s3_bucket" "manual" {
+  bucket = "terraformthalles"
+
+  tags = {
+    data = "08/12/21"
+    importador = "12/12/21"
+    ManagedBy = "Terraform"
+  }
+}
+
 resource "aws_s3_bucket_object" "this" {
   bucket = aws_s3_bucket.this.bucket
   key    = "config/${local.ip_filepatch}"
   source = local.ip_filepatch
   etag   = filemd5(local.ip_filepatch)
+  content_type =  "application/json"
 
   tags = local.common_tags
 
@@ -19,6 +30,7 @@ resource "aws_s3_bucket_object" "random" {
   key    = "config/${random_pet.bucket.id}.json"
   source = local.ip_filepatch
   etag   = filemd5(local.ip_filepatch)
+  content_type =  "application/json"
 
   tags = local.common_tags
 
